15c00e12561ca00ac7efc753e615971aef921d54
laisflaviosilva <laisflavio.silva@gmail.com>
2020-09-18 18:01:16 -0300

    Continuacao do projeto

--- src/endpoints/getRecipes.ts
+++ src/endpoints/getRecipes.ts
@@ -6,12 +6,13 @@ import { Authenticator } from '../services/Authenticator'
 export const getRecipes = async(req: Request, res: Response) => {
     try {
         const token = req.headers.authorization as string
+        const id = req.params.id
 
         const authenticator = new Authenticator()
         const authenticatorData = authenticator.getData(token)
 
         const recipeDatabase = new RecipesDatabase()
-        const recipe = await recipeDatabase.getRecipesForId(authenticatorData.id)
+        const recipe = await recipeDatabase.getRecipesForId(id)
 
 
         if(!recipe) {
